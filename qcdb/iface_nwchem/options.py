e5c91125e0dc4a1201495f1193622d28bbe13564
Annabelle Lolinco <alolinco@iastate.edu>
2018-06-26 16:00:01 -0500

    start options.py for nwchem interface

---
+++ qcdb/iface_nwchem/options.py
@@ -0,0 +1,11 @@
+import collections
+
+from ..moptions.read_options2 import RottenOption
+
+### Example of options 
+#options.add('nwchem', RottenOption(
+    #keyword = name within NWCHEM documentation
+    #default = psi4 options from common-driver-psi4
+    #validator = either a parser or lambda transforming it to uppercase or something similar
+    #glossary = description
+
