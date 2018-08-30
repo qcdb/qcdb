29749945308dca9d91652d13cfd565830f489815
Annabelle Lolinco <alolinco@iastate.edu>
2018-08-30 14:25:22 -0500

    add JYL nwc tests and made prelim rohf scf test to run

--- tests/nwchem_tests/cc1/test_cc1_h2o.py
+++ tests/nwchem_tests/cc1/test_cc1_h2o.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import qcdb
 from utils import *
 from addons import *
 
@@ -10,9 +11,9 @@ h2o = qcdb.set_molecule("""
     H 1 0.97 2 103.0
     """)
 print(h2o)
-print(qcdb.get_active_options.print_changed())
+print(qcdb.get_active_options())
 
-qcdb.set_options({basis: '6-31G**'})
+qcdb.set_options({'basis': '6-31G**'})
 
 qcdb.energy('ccsd')
 
@@ -21,8 +22,8 @@ refscf   = -76.0229427274435  #TEST
 refccsd  = -0.20823570806196  #TEST
 reftotal = -76.2311784355056  #TEST
 
-assert compare_values(refnuc,   h2o.nuclear_repulsion_energy(),          3, "Nuclear repulsion energy") #TEST
-assert compare_values(refscf,   qcdb.get_variable("SCF total energy"),        5, "SCF energy")               #TEST
-assert compare_values(refccsd,  qcdb.get_variable("CCSD correlation energy"), 4, "CCSD contribution")        #TEST
-assert compare_values(reftotal, qcdb.get_variable("Current energy"),          7, "Total energy")             #TEST
+assert compare_values(refnuc, h2o.nuclear_repulsion_energy(), 3, "Nuclear repulsion energy") #TEST
+assert compare_values(refscf, qcdb.get_variable("SCF total energy"), 5, "SCF energy")        #TEST
+assert compare_values(refccsd,qcdb.get_variable("CCSD correlation energy"), 4, "CCSD contribution")        #TEST
+assert compare_values(reftotal, qcdb.get_variable("Current energy"),7, "Total energy") #TEST
 
