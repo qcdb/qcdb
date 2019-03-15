e1d8d6007dcd3e03d25b46c055c7bfcf4ec17e19
Annabelle Lolinco <alolinco@iastate.edu>
2019-03-15 14:39:45 -0500

    edit to distinguish tce vs non-tce cc tests, add unchecked nwchem tests to .gitignore

--- tests/nwchem_tests/test_sp_rhf_ccsd_t-2.py
+++ tests/nwchem_tests/test_sp_rhf_ccsd_t-2.py
@@ -16,8 +16,7 @@ h2o = qcdb.set_molecule('''
 print(h2o)
 
 
-def check_ccsd_t_2(return_value, is_df, is_5050):
-    if is_df:
+def check_ccsd_t_2(return_value, is_5050):
         ref = -76.026760737428
         nre = 9.187334240165
         ccsd_corr = -0.213341272556805
@@ -28,41 +27,25 @@ def check_ccsd_t_2(return_value, is_df, is_5050):
         mp2tot = -76.230777733731884
         ss = -0.046033728720216 * 1.130000000000000
         os = -0.167307543836588 * 1.270000000000000
-        ccsd_t_corl = -0.003059541651480
         ccsd_t_tot = -76.243161551636248
         t_corr = -0.003145008014227
+        ccsd_t_corl= ccsd_corr + t_corr
         a5050corl = 0.5 * (ss + os)
         a5050tot = a5050corl + scstot
-    else:
-        ref = -76.026760737427963
-        nre = 9.187334240165
-        ccsd_corr = -0.213341272556805
-        ccsd_tot = -76.240102009984767
-        scscorl = -0.264498694126312
-        scstot = -76.291259431554266
-        mp2corl = -0.204016996303923
-        mp2tot = -76.230777733731884
-        ss = -0.046033728720216
-        os = -0.167307543836588
-        t_corr = -0.003059541651480
-        ccsd_t_tot = -76.243247017998996
-        ccsd_t_corl = -0.003059541651480
-        a5050corl = 0.5 * (ss + os)
-        a5050tot = a5050corl + ref
-
-    assert compare_values(ref, qcdb.get_variable('HF TOTAL ENERGY'), 5, 'hf ref')
-    assert compare_values(nre, qcdb.get_variable('NUCLEAR REPULSION ENERGY'), 5, 'nre')
-    assert compare_values(mp2corl, qcdb.get_variable('MP2 CORRELATION ENERGY'), 5, 'mp2 corl')
-    assert compare_values(mp2tot, qcdb.get_variable('MP2 TOTAL ENERGY'), 5, 'mp2 tot')
+        
+        assert compare_values(ref, qcdb.get_variable('HF TOTAL ENERGY'), 5, 'hf ref')
+        assert compare_values(nre, qcdb.get_variable('NUCLEAR REPULSION ENERGY'), 5, 'nre')
+        assert compare_values(mp2corl, qcdb.get_variable('MP2 CORRELATION ENERGY'), 5, 'mp2 corl')
+        assert compare_values(mp2tot, qcdb.get_variable('MP2 TOTAL ENERGY'), 5, 'mp2 tot')
     #assert compare_values(scscorl, qcdb.get_variable('SCS-CCSD CORRELATION ENERGY'), 5, 'scs ccsd corl')
     #assert compare_values(scstot, qcdb.get_variable('SCS-CCSD TOTAL ENERGY'), 5, 'scs ccsd tot')
-    assert compare_values(ccsd_t_corl, qcdb.get_variable('CCSD(T) CORRELATION ENERGY'), 5, 'ccsd(t) corl')
-    assert compare_values(ccsd_t_tot, qcdb.get_variable('CCSD(T) TOTAL ENERGY'), 5, 'ccsd(t) tot')
-    if is_5050:
-        assert compare_values(a5050corl, qcdb.get_variable('CUSTOM SCS-CCSD CORRELATION ENERGY'), 5, 'mp2 scscorl')
-        assert compare_values(a5050tot, qcdb.get_variable('CUSTOM SCS-CCSD TOTAL ENERGY'), 5, 'mp2 scstot')
-    assert compare_values(mp2tot, return_value, 5, 'mp2 return')
-    assert compare_values(t_corr, qcdb.get_variable('(T) CORRECTION ENERGY'), 5, 'ccsd(t) correction')
+        assert compare_values(ccsd_t_corl, qcdb.get_variable('CCSD(T) CORRELATION ENERGY'), 5, 'ccsd(t) corl')
+        assert compare_values(ccsd_t_tot, qcdb.get_variable('CCSD(T) TOTAL ENERGY'), 5, 'ccsd(t) tot')
+        if is_5050:
+            assert compare_values(a5050corl, qcdb.get_variable('CUSTOM SCS-CCSD CORRELATION ENERGY'), 5, 'mp2 scscorl')
+            assert compare_values(a5050tot, qcdb.get_variable('CUSTOM SCS-CCSD TOTAL ENERGY'), 5, 'mp2 scstot')
+        assert compare_values(mp2tot, return_value, 5, 'mp2 return')
+        assert compare_values(t_corr, qcdb.get_variable('(T) CORRECTION ENERGY'), 5, 'ccsd(t) correction')
 
 
 @using_nwchem
@@ -79,7 +62,7 @@ def test_1_hf():
     })
     print("Testing hf...")
     val = qcdb.energy('nwc-ccsd(t)')
-    check_ccsd_t_2(val, is_df=True, is_5050=False)
+    check_ccsd_t_2(val, is_5050=False)
 
 
 @using_nwchem
@@ -96,7 +79,7 @@ def test_2_mp2():
     })
     print("Testing MP2...")
     val = qcdb.energy('nwc-ccsd(t)')
-    check_ccsd_t_2(val, is_df=True, is_5050=False)
+    check_ccsd_t_2(val, is_5050=False)
 
 
 @using_nwchem
@@ -113,7 +96,7 @@ def test_3_mp2_5050():
     })
     print("testing mp2...")
     val = qcdb.energy('nwc-ccsd(t)')
-    check_ccsd_t_2(val, is_df=False, is_5050=True)
+    check_ccsd_t_2(val, is_5050=True)
 
 
 @using_nwchem
@@ -130,38 +113,5 @@ def test_4_ccsd():
     })
     print("Testing CCSD energy ...")
     val = qcdb.energy('nwc-ccsd')
-    check_ccsd_t_2(val, is_df=True, is_5050=False)
+    check_ccsd_t_2(val, is_5050=False)
 
-
-@using_nwchem
-def test_5_ccsd_t():
-    qcdb.set_options({
-        'basis': 'cc-pvdz',
-        'memory': '600 mb',
-        'nwchem_scf': 'rhf',
-        'nwchem_scf_thresh': 1.0e-12,
-        'nwchem_tce_dft': False,
-        'nwchem_tce_module': 'ccsd(t)',
-        'nwchem_tce': False,
-        'nwchem_tce_thresh': 1.0e-12
-    })
-    print("Testing CCSD(T) energy ...")
-    val = qcdb.energy('nwc-ccsd(t)')
-    check_ccsd_t_2(val, is_df=True, is_5050=False)
-
-
-@using_nwchem
-def test_6_custom_ccsd():
-    qcdb.set_options({
-        'basis': 'cc-pvdz',
-        'memory': '600 mb',
-        'nwchem_scf': 'rhf',
-        'nwchem_scf_thresh': 1.0e-12,
-        'nwchem_tce_dft': False,
-        'nwchem_tce_module': 'ccsd(t)',
-        'nwchem_tce': True,
-        'nwchem_tce_thresh': 1.0e-12
-    })
-    print("Testing custom SCS MP2 energy ...")
-    val = qcdb.energy('nwc-ccsd(t)')
-    check_ccsd_t_2(val, is_df=False, is_5050=True)
