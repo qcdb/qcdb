083792ef385440c391b40032fb9ecb986aa7889c
Annabelle Lolinco <alolinco@iastate.edu>
2019-05-10 17:29:43 -0500

    hf portion of nwc tests passing of dft and update of grad hf test

--- tests/nwchem_tests/test_rhf_scf_grad.py
+++ tests/nwchem_tests/test_rhf_scf_grad.py
@@ -6,6 +6,7 @@ from ..utils import *
 from ..addons import *
 import qcdb
 import numpy as np
+import pprint
 
 h2o = qcdb.set_molecule('''
         O     0.000000000000    0.000000000000   -0.065638538099
@@ -22,18 +23,21 @@ def check_hf(return_value):
         [0.000000, 0.010677, 0.007595]])
     assert compare_values(ref, qcdb.get_variable('HF TOTAL ENERGY'), 5, 'hf ref')
     assert compare_values(nre, qcdb.get_variable('NUCLEAR REPULSION ENERGY'), 5, 'nre')
+    pprint.pprint(grads)
+    pprint.pprint(qcdb.get_variable('CURRENT GRADIENT'))
+    
     assert compare_arrays(grads, qcdb.get_variable('CURRENT GRADIENT'), 5, 'hf grad')
-
 @using_nwchem
 def test_1_hf():
     qcdb.set_options({
         'basis': 'cc-pvdz',
         'memory': '4000 mb',
         'nwchem_scf__rhf': True,
-        'nwchem_scf__thresh': 1.0e-8,
-        #'nwchem_scf__nopen': 0,  # invalid value
+        'scf__e_convergence': 1.0e-8,
+        'nwchem_scf__nopen': 0,
         #'nwchem_task_hf'   : 'gradient'
     })
     print('Testing hf...')
     val = qcdb.gradient('nwc-hf')
     check_hf(val)
+    pprint.pprint(val)
