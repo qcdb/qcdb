ffa9bcce74ee9a29f7adedbf212422e0745c4e7c
loriab <lori.burns@gmail.com>
2021-09-10 14:30:06 +0000

    Deploying to gh-pages from @ qcdb/qcdb@505b4c733647445f9c9cfa04382c7be625874a45 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g9c98285',
+    VERSION: '0+untagged.1.g505b4c7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
