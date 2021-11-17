8ffa9b1d8203df83b29c83b63df005802950b8a3
loriab <loriab@users.noreply.github.com>
2021-11-17 02:50:18 +0000

    Deploying to gh-pages from @ qcdb/qcdb@5fdf5989b5fc7136a416bb86e83cd327a3768260 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g066f39e',
+    VERSION: '0+untagged.1.g5fdf598',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
